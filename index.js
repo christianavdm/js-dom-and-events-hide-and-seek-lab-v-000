04bf490398d199711b99d75ef5cf310afe1cf53f
christianavdm <christianavdm@gmail.com>
2019-07-25 13:32:04 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getFirstSelector(selector) {
 function nestedTarget() {
     return document.querySelector('#nested .target')
 }
+
+function deepestChild() {
+    return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+    const rankedLists = document.getElementsByClassName('ranked-list'); 
+    let li = rankedLists[0]; 
+    li.textContent = li.textContent * n; 
+
+    let liOne = rankedLists[1]; 
+    liOne.textContent = li.textContent * n; 
+}
